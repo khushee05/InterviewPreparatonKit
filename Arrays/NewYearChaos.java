ac9f214ddbac5934b3618646e75ef61a7259938f
Khushee Jain <78522883+khushee05@users.noreply.github.com>
2022-03-01 16:31:44 +0530

    Create NewYearChaos.java

---
+++ Arrays/NewYearChaos.java
@@ -0,0 +1,79 @@
+/* 
+
+It is New Year's Day and people are in line for the Wonderland rollercoaster ride. Each person wears a sticker indicating their initial position in the queue from 1 to n. Any person can bribe the person directly in front of them to swap positions, but they still wear their original sticker. One person can bribe at most two others.
+
+Determine the minimum number of bribes that took place to get to a given queue order. Print the number of bribes, or, if anyone has bribed more than two people, print Too chaotic.
+
+Sample Input
+
+STDIN       Function
+-----       --------
+2           t = 2
+5           n = 5
+2 1 5 3 4   q = [2, 1, 5, 3, 4]
+5           n = 5
+2 5 1 3 4   q = [2, 5, 1, 3, 4]
+Sample Output
+
+3
+Too chaotic
+*/
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+
+
+    public static void minimumBribes(List<Integer> q) {
+    
+    int b =0;
+    for(int i=q.size(); i>=1; --i){
+        if(q.get(i-1)-i>2){
+            System.out.println("Too chaotic");
+            return;
+        }
+        for(int j = i-2; j>=Math.max(q.get(i-1)-2,0); j--){
+            if(q.get(j)>q.get(i-1))
+            b++;
+        }
+    }
+    System.out.println(b);
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<Integer> q = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                    .map(Integer::parseInt)
+                    .collect(toList());
+
+                Result.minimumBribes(q);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+    }
+}
